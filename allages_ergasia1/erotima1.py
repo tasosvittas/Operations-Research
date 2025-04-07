c42df6d7d62d458baa3ef7fa5f008d0f2dbc1ab7
tasosvittas <tasosvittas@gmail.com>
2025-04-07 19:14:40 +0300

    changes on another file

---
+++ allages_ergasia1/erotima1.py
@@ -0,0 +1,86 @@
+import os
+import time
+import numpy as np
+from ortools.linear_solver import pywraplp
+
+def read_file(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+    n = int(lines[0].strip())
+    matrix = []
+    all_costs = []
+    for line in lines[1:]:
+        numbers = line.split()          # Χωρίζει τη γραμμή σε ξεχωριστούς αριθμούς (ως strings)
+        for num in numbers:
+            all_costs.append(int(num))  # Μετατρέπει κάθε αριθμό σε int και τον προσθέτει στη λίστα
+    for i in range(n):
+        matrix.append(all_costs[i * n:(i + 1) * n])
+    return np.array(matrix)
+
+def assignment_problem_solver(jobs_matrix):
+    workers = len(jobs_matrix[0])
+    jobs = len(jobs_matrix)
+
+    solver = pywraplp.Solver.CreateSolver("SCIP")
+    if not solver:
+        return
+    
+    x={}
+    for i in range(jobs):
+        for j in range(workers):
+            x[i,j] = solver.IntVar(0, 1, "")
+
+    for i in range(jobs):
+        solver.Add(solver.Sum([x[i, j] for j in range(workers)]) <= 1)
+
+    for j in range(workers):
+        solver.Add(solver.Sum([x[i, j] for i in range(jobs)]) == 1)
+
+    assigments = []
+    for i in range(jobs):
+        for j in range(workers):
+            assigments.append(jobs_matrix[i][j] * x[i, j])
+    solver.Minimize(solver.Sum(assigments))
+
+    start_time = time.time()
+    status = solver.Solve()
+    finish_time = time.time()
+
+    if status == pywraplp.Solver.OPTIMAL or status == pywraplp.Solver.FEASIBLE:
+        total_cost = solver.Objective().Value()
+        assignments = []
+        for i in range(jobs):
+            for j in range(workers):
+                if x[i,j].solution_value() > 0.5:
+                    assignments.append((i, j, jobs_matrix[i][j]))
+        return total_cost, assignments, finish_time-start_time
+    else:
+        return None, None, finish_time - start_time    
+
+
+def write_solution(file_path, total_cost, assignments):
+    with open(file_path, 'w') as file:
+        file.write(f"{total_cost}\n")
+        for assignment in assignments:
+            file.write(f"{assignment[0]},{assignment[1]},{assignment[2]}\n")
+
+def main():
+    files = [
+        "dataset/assign100.txt", "dataset/assign200.txt", "dataset/assign300.txt",
+        "dataset/assign400.txt", "dataset/assign500.txt", "dataset/assign600.txt",
+        "dataset/assign700.txt", "dataset/assign800.txt"
+    ]
+
+    output_dir = "solutions/erotima1"
+    for file in files:
+        filename = os.path.basename(file)
+        jobs_matrix = read_file(file)
+        total_cost, assignments, solve_time = assignment_problem_solver(jobs_matrix)
+        solution_file = file.replace(".txt", "_solution.txt")
+        solution_file = os.path.join(output_dir, filename.replace(".txt", "_solution.txt"))
+        write_solution(solution_file, total_cost, assignments)
+        print(assignments)
+        print(f"Solved {file}: Total Cost = {total_cost}, Time = {solve_time:.2f} seconds")
+
+if __name__ == "__main__":
+    main()
