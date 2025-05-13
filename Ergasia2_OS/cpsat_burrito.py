65c5cee3d690134d7354326485c6dd1df0a2e9ae
tasosvittas <tasosvittas@gmail.com>
2025-05-13 19:55:26 +0300

    texniki anafora & code

---
+++ Ergasia2_OS/cpsat_burrito.py
@@ -0,0 +1,5 @@
+from ortools.sat.python import cp_model
+from read_dataset import load_data
+import pandas as pd
+
+print(load_data())
---
+++ Ergasia2_OS/read_dataset.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def load_data():
+    start_date = 1
+    end_date = 5
+    for i in range(start_date, end_date):
+        demand_nodes = pd.read_csv(f'burrito_dataset/scenDE4BD4_round1_day{i}_demand_node_data.csv')
+        truck_assignments = pd.read_csv(f'burrito_dataset/scenDE4BD4_round1_day{i}_demand_truck_data.csv')
+        problem_data = pd.read_csv(f'burrito_dataset/scenDE4BD4_round1_day{i}_problem_data.csv')
+
+        print(demand_nodes,truck_assignments,problem_data)
+    return demand_nodes, truck_assignments, problem_data
+
+
+# if __name__ == '__main__':
+#     load_data()
